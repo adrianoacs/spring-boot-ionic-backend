4a21e3c1652607c96f12bb76dff8e53e545b95df
Adriano Couto de Souza <asouza@trendoperadora.com.br>
2025-03-03 20:34:57 -0300

    estado e cidade

--- src/main/java/com/nelioalves/cursomc/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/cursomc/CursomcApplication.java
@@ -1,8 +1,12 @@
 package com.nelioalves.cursomc.cursomc;
 
 import com.nelioalves.cursomc.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.cursomc.domain.Cidade;
+import com.nelioalves.cursomc.cursomc.domain.Estado;
 import com.nelioalves.cursomc.cursomc.domain.Produto;
 import com.nelioalves.cursomc.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.cursomc.repositories.CidadeRepository;
+import com.nelioalves.cursomc.cursomc.repositories.EstadoRepository;
 import com.nelioalves.cursomc.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -24,6 +28,12 @@ public class CursomcApplication implements CommandLineRunner {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -33,6 +43,15 @@ public class CursomcApplication implements CommandLineRunner {
         var produto1 = new Produto(null, "Computador", 2000.00);
         var produto2 = new Produto(null, "Impressora", 800.00);
         var produto3 = new Produto(null, "Mouse", 80.00);
+
+        var est2 = new Estado(null, "São Paulo");
+        var est1 = new Estado(null, "Minas Gerais");
+        var c2 = new Cidade(null, "São Paulo", est2);
+        var c3 = new Cidade(null, "Campinas", est2);
+        var c1 = new Cidade(null, "Uberlandia", est1);
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
+
 
 
 
@@ -45,7 +64,7 @@ public class CursomcApplication implements CommandLineRunner {
 
         categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
         produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
-
-
+        estadoRepository.saveAll(Arrays.asList(est2, est1));
+        cidadeRepository.saveAll(Arrays.asList(c2, c3, c1));
     }
 }
