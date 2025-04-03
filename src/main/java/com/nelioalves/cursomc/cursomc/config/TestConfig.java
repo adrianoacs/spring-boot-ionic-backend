790f276c31efbd2c606b32ddb36e19c3ed9345bc
Adriano Couto de Souza <asouza@trendoperadora.com.br>
2025-04-03 18:57:34 -0300

    adição @Bean no método 'instantiateDatabase' na classe de configuração 'TestConfig'

--- src/main/java/com/nelioalves/cursomc/cursomc/config/TestConfig.java
+++ src/main/java/com/nelioalves/cursomc/cursomc/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.nelioalves.cursomc.cursomc.config;
 
 import com.nelioalves.cursomc.cursomc.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -12,6 +13,7 @@ public class TestConfig {
     @Autowired
     DBService service;
 
+    @Bean
     public boolean instantiateDatabase() throws Exception {
         service.instantiateDatabase();
         return true;
